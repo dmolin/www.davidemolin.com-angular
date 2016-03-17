d7ac03511bf6528e4b90ce467adf4e80e99c62c8
dmolin <davide.molin@gmail.com>
2016-03-17 08:06:51 +0000

    small updates

---
+++ src/app/recommendations/directives/dmRecommendationsSpec.js
@@ -0,0 +1,10 @@
+describe('dmRecommendations', function() {
+    var scope, subject, tu;
+
+    beforeEach(function() {
+        module('app');
+        inject(function($rootScope, $compile) {
+
+        });
+    })
+});
